d5d29d3cfbb1cffd039a9602088d9479947d471a
Maxwell Krohn <themax@gmail.com>
2012-06-13 13:57:11 -0400

    fuuuuuuuuck

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
-index.html : index-in.html lib.js
+default: index.html
+all: index.html
+
+lib-min.js : lib.js
+	uglifyjs < $< > $@
+index.html : index-in.html lib-min.js make.py
 	python make.py < $< > $@
--- make.py
+++ make.py
@@ -2,9 +2,19 @@
 import sys
 import re
 
-rxx = re.compile("%\{include\\s+(.*?)\}")
-for l in sys.stdin.readlines():
-    m = rxx.match (l)
-    if m:
-        file = m.group(1)
-        print m
+def process_handle (h):
+    rxx = re.compile(r'^(.*)\{%\s?include\s+(.*?)%\}(.*)$');
+    for l in h.readlines():
+        m = rxx.match(l)
+        if m:
+            pre = m.group(1)
+            file = m.group(2).strip()
+            post = m.group(3)
+            sys.stdout.write(pre)
+            nh = open (file, "r")
+            process_handle (nh)
+            sys.stdout.write(post)
+        else:
+            sys.stdout.write(l)
+
+process_handle (sys.stdin)
