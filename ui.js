afa6c387a98dfe344b943a8542a557bac6140592
Maxwell Krohn <themax@gmail.com>
2012-06-17 21:18:30 -0400

    better, more progress

--- ui.js
+++ ui.js
@@ -1,7 +1,11 @@
-function turnBlack (event) {
+
+var inputs={};
+
+function acceptInput (event, fieldname) {
     var se = event.srcElement;
     if (!se.className.match("input-black")) {
         event.srcElement.className += " input-black";
         event.srcElement.value = "";
+        inputs[fieldname] = 1;
     }
 }
