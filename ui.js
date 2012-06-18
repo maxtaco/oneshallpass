407d9e068fbc54d6994dfc6ecf070977bb1629ab
Maxwell Krohn <themax@gmail.com>
2012-06-17 23:05:12 -0400

    uggies

--- ui.js
+++ ui.js
@@ -1,11 +1,18 @@
 
 var inputs={};
 
-function acceptInput (event, fieldname) {
+function acceptInput (event) { 
     var se = event.srcElement;
     if (!se.className.match("input-black")) {
         event.srcElement.className += " input-black";
         event.srcElement.value = "";
-        inputs[fieldname] = 1;
+        inputs[se.id.split('-')[1]] = 1;
     }
+    swizzle();
+}
+
+function swizzle () { 
+    if (inputs.passphrase && inputs.domain && inputs.email) {
+    }
+    return 0;
 }
