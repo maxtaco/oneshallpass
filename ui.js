78f432ff6cd92b91c10deb6e9669524dfa33b822
Maxwell Krohn <themax@gmail.com>
2012-07-22 21:58:08 -0400

    mess around with metaport a bit

--- ui.js
+++ ui.js
@@ -213,9 +213,6 @@ function domobiles() {
     
     var mobsafari = (/iphone|ipad|ipod/i.test(navigator.userAgent.toLowerCase()));
     var mobile =(/android|blackberry/i.test(navigator.userAgent.toLowerCase()));
-    if (!mobile && !mobsafari) {
-	$('frontbox').style.width =  "558px";
-    }
     if (mobsafari) {
 	$('email').type = "email";
     }
